bb52c480f7408ed49f4d91f174cc28f7cf1beca0
ujhrkzy <kazuyaujihiro@gmail.com>
2015-09-06 21:17:16 +0900

    add new view

--- MusicCapture/src/com/ujhrkzy/musiccapture/CameraPreview.java
+++ MusicCapture/src/com/ujhrkzy/musiccapture/CameraPreview.java
@@ -1,6 +1,7 @@
 package com.ujhrkzy.musiccapture;
 
 import java.io.IOException;
+import java.util.List;
 
 import android.content.Context;
 import android.hardware.Camera;
@@ -10,16 +11,14 @@ import android.view.SurfaceView;
 public class CameraPreview extends SurfaceView implements
         SurfaceHolder.Callback {
 
-    private Camera mCam;
+    private Camera camera;
 
     /**
      * コンストラクタ
      */
     public CameraPreview(Context context, Camera cam) {
         super(context);
-
-        mCam = cam;
-
+        camera = cam;
         // サーフェスホルダーの取得とコールバック通知先の設定
         SurfaceHolder holder = getHolder();
         holder.addCallback(this);
@@ -32,11 +31,12 @@ public class CameraPreview extends SurfaceView implements
     public void surfaceCreated(SurfaceHolder holder) {
         try {
             // カメラインスタンスに、画像表示先を設定
-            mCam.setPreviewDisplay(holder);
+            camera.setPreviewDisplay(holder);
             // プレビュー開始
-            mCam.startPreview();
+            camera.startPreview();
         } catch (IOException e) {
             //
+            e.printStackTrace();
         }
     }
 
@@ -44,6 +44,12 @@ public class CameraPreview extends SurfaceView implements
      * SurfaceView 破棄
      */
     public void surfaceDestroyed(SurfaceHolder holder) {
+        if (camera != null) {
+            camera.setPreviewCallback(null);
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
     }
 
     /**
@@ -51,7 +57,49 @@ public class CameraPreview extends SurfaceView implements
      */
     public void surfaceChanged(SurfaceHolder holder, int format, int width,
             int height) {
-        // 画面回転に対応する場合は、ここでプレビューを停止し、
-        // 回転による処理を実施、再度プレビューを開始する。
+        // 最適サイズを取得
+        // Camera.Parameters params = mCam.getParameters();
+        // List<Camera.Size> sizes = params.getSupportedPreviewSizes();
+        // Camera.Size optimalSize = getOptimalPreviewSize(sizes, width,
+        // height);
+        // params.setPreviewSize(optimalSize.width, optimalSize.height);
+        // mCam.setParameters(params);
+        //
+        // // プレビュースタート（Changedは最初にも1度は呼ばれる）
+        camera.startPreview();
+    }
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w,
+            int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio = (double) h / w;
+
+        if (sizes == null)
+            return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE)
+                continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
     }
 }
--- MusicCapture/src/com/ujhrkzy/musiccapture/MainActivity.java
+++ MusicCapture/src/com/ujhrkzy/musiccapture/MainActivity.java
@@ -1,147 +1,48 @@
 package com.ujhrkzy.musiccapture;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.hardware.Camera;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.provider.MediaStore.Images;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.MotionEvent;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnTouchListener;
-import android.widget.FrameLayout;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity implements OnClickListener {
 
-    // カメラインスタンス
-    private Camera mCam = null;
-
-    // カメラプレビュークラス
-    private CameraPreview mCamPreview = null;
-
-    // 画面タッチの2度押し禁止用フラグ
-    private boolean mIsTake = false;
-
-    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // カメラインスタンスの取得
-        try {
-            mCam = Camera.open();
-        } catch (Exception e) {
-            // エラー
-            this.finish();
-        }
-
-        // FrameLayout に CameraView クラスを設定
-        FrameLayout preview = (FrameLayout) findViewById(R.id.cameraPreview);
-        // findViewById(R.id.cameraPreview);
-        mCamPreview = new CameraPreview(this, mCam);
-        preview.addView(mCamPreview);
-
-        // mCamPreview に タッチイベントを設定
-        mCamPreview.setOnTouchListener(new OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    if (!mIsTake) {
-                        // 撮影中の2度押し禁止用フラグ
-                        mIsTake = true;
-                        // 画像取得
-                        mCam.takePicture(null, null, mPicJpgListener);
-                    }
-                }
-                return true;
-            }
-        });
+        Button btn = (Button) findViewById(R.id.caputure);
+        btn.setOnClickListener(this);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        // カメラ破棄インスタンスを解放
-        if (mCam != null) {
-            mCam.release();
-            mCam = null;
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
-    /**
-     * JPEG データ生成完了時のコールバック
-     */
-    private Camera.PictureCallback mPicJpgListener = new Camera.PictureCallback() {
-        public void onPictureTaken(byte[] data, Camera camera) {
-            if (data == null) {
-                return;
-            }
-
-            String saveDir = Environment.getExternalStorageDirectory()
-                    .getPath() + "/test";
-
-            // SD カードフォルダを取得
-            File file = new File(saveDir);
-
-            // フォルダ作成
-            if (!file.exists()) {
-                if (!file.mkdir()) {
-                    Log.e("Debug", "Make Dir Error");
-                }
-            }
-
-            // 画像保存パス
-            Calendar cal = Calendar.getInstance();
-            SimpleDateFormat sf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-            String imgPath = saveDir + "/" + sf.format(cal.getTime()) + ".jpg";
-
-            // ファイル保存
-            FileOutputStream fos;
-            try {
-                fos = new FileOutputStream(imgPath, true);
-                fos.write(data);
-                fos.close();
-
-                // アンドロイドのデータベースへ登録
-                // (登録しないとギャラリーなどにすぐに反映されないため)
-                registAndroidDB(imgPath);
-
-            } catch (Exception e) {
-                Log.e("Debug", e.getMessage());
-            }
-
-            fos = null;
-
-            // takePicture するとプレビューが停止するので、再度プレビュースタート
-            mCam.startPreview();
-
-            mIsTake = false;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
         }
-    };
+        return super.onOptionsItemSelected(item);
+    }
 
-    /**
-     * アンドロイドのデータベースへ画像のパスを登録
-     * 
-     * @param path
-     *            登録するパス
-     */
-    private void registAndroidDB(String path) {
-        // アンドロイドのデータベースへ登録
-        // (登録しないとギャラリーなどにすぐに反映されないため)
-        ContentValues values = new ContentValues();
-        ContentResolver contentResolver = MainActivity.this
-                .getContentResolver();
-        values.put(Images.Media.MIME_TYPE, "image/jpeg");
-        values.put("_data", path);
-        contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                values);
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, CaputureActivity.class);
+        // 次画面のアクティビティ起動
+        startActivity(intent);
     }
 }
